41512674a7081e34b8ccd2ad1b1f18039982890c
Rickelme Dias <rickelmegadias@gmail.com>
2023-03-22 19:36:24 -0300

    Jump Search changes

--- Resources/Code/Data-Structure/Search-Algorithms/Jump-Search/python/JumpSearch.py
+++ Resources/Code/Data-Structure/Search-Algorithms/Jump-Search/python/JumpSearch.py
@@ -4,15 +4,15 @@ class Search:
   def __init__(self, array_of_numbers):
     self.array_of_numbers = array_of_numbers
 
-  def jump(self, number_to_find):
+  def jump_search(self, number_to_find):
     array_size=len(self.array_of_numbers)
-    blockSize=int(array_size**(1/2)) # int(math.sqrt(array_size))
+    stepSize=int(array_size**(1/2)) # int(math.sqrt(array_size))
     start=0
-    end=blockSize
+    end=stepSize
 
     while end < array_size - 1 and array_of_numbers[end] <= number_to_find:
         start = end
-        end = end + blockSize
+        end = end + stepSize
         if end > array_size - 1:
             end = array_size - 1
 
@@ -26,4 +26,4 @@ class Search:
 array_of_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 search = Search(array_of_numbers)
 number_to_find = int(input('What number do you want find?: '))
-print(search.jump(number_to_find))
+print(search.jump_search(number_to_find))
